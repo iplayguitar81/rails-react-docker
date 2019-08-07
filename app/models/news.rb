829671530d37e2279994dc68c2a4de73213f869a
Chris Snyder <chris.snyder@triadhq.com>
2019-08-07 11:25:21 -0700

    added minor front end improvements and form validation to news model

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,6 @@
 class News < ApplicationRecord
+	#validate presence of title, subtitle, and body to prevent article creation if not entered into form...
+	validates :title, :subtitle, :body, presence: true
 
 has_one_attached :image
 
